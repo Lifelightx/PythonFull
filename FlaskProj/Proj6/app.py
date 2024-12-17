080cbd38021d39d986b6cdbbf74a48fa4b33bfbb
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-12-17 08:08:05 +0530

    update

---
+++ FlaskProj/Proj6/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+import MySQLdb
+import  bcrypt
+
+conn = MySQLdb.connect(host='localhost', user='root', database='sindhunibas', password='Jeeban@12345')
+
+cursor = conn.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS websites (email VARCHAR(20) PRIMARY KEY, password VARCHAR(255))')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/signup', methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        print("Exucuted")
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+        hashpassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        try:
+            cursor.execute("insert into websites(email, password) values(%s, %s)",(email,hashpassword.decode('utf-8')))
+            conn.commit()
+            return jsonify({'success':True})
+        except Exception as e:
+            conn.rollback()
+            print("Exception: ", e)
+        return jsonify({"success":False})
+        
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    cursor.execute('Select * from websites where email = %s',(email,))
+    user = cursor.fetchone()
+    print(user)
+    if not user:
+        return jsonify({'success':False, 'message':"Invalid Credential"}) , 401
+    if not bcrypt.checkpw(password.encode('utf-8'), user[1].encode('utf-8')):
+        return jsonify({'success':False, 'message':"Email and Password are incorrect"}), 401
+    return jsonify({'success':True, 'message':"User LogedIn"}) , 200
+        
+               
+            
+        
+    
+if __name__ == '__main__':
+    app.run(debug=True)
