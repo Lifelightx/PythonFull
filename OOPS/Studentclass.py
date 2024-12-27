1b3b3c600ca5d860d598e35c78d7a098de247ccf
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-12-27 12:44:24 +0530

    class

---
+++ OOPS/Studentclass.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init(self):
+        self.name = ""
+        self.age = 0
+        self.grade = ""
+    def showDetails(self):
+        print("Name : ", self.name)
+        print("Age : ", self.age)
+        print("Grade : ", self.grade)
+
+
+s1 = Student()
+s1.name = "Jeeban"
+s1.age = 12
+s1.grade = "A"
+
+s1.showDetails()
