23d6201cb4ffe8ce4f7160d4d14e29d9b5743cd5
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-12-27 19:53:22 +0530

    update

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+my_dict = {"rama":"1st Jan", "sita":"24th Feb", "Laxman":"12th March"}
+
+print(my_dict.keys())
+print(my_dict.values())
+
+my_dict["Laxman"] = "1st Nov"
+print(my_dict)
+print(my_dict["rama"])
+print(my_dict.items())
+
+for i in my_dict.keys():
+    print(my_dict[i])
+    
+for i,k in my_dict.items():
+    print(i,k)
