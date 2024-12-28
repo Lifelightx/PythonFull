4064771f6da5ebd8fabefe6cced1772710f56688
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-12-28 14:56:11 +0530

    update

--- DSA/bubbleSort.py
+++ DSA/bubbleSort.py
@@ -18,3 +18,16 @@ for i in range(len(arr)):
             arr[j],arr[j+1] = arr[j+1],arr[j]
             
 print(arr)
+
+#selection sort
+
+arr = [2,5,1,9,3,7]
+
+for i in range(len(arr)):
+    min_index = i
+    for j in range(i+1,len(arr)):
+        if arr[j]<arr[min_index]:
+            min_index = j
+    arr[i],arr[min_index] = arr[min_index],arr[i]
+    
+print(arr)
