8d33bf900b464579155e66006c193757624bd594
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-12-29 16:32:20 +0530

    update

--- DSA/ARRAY/insertAnElement.py
+++ DSA/ARRAY/insertAnElement.py
@@ -8,9 +8,21 @@ new_arr = [0]*(len(arr)+1)
 for i in range(pos):
     new_arr[i] = arr[i]
 new_arr[pos] = ele
-for i in range(pos,len(new_arr)):
+for i in range(pos,len(arr)):
     new_arr[i+1] = arr[i]
 
 print(new_arr)
 
-arr.insert()
+#without creating a new array
+
+narr = [2,4,1,6,7,9]
+
+#adding a dummy value for increase the size
+narr.append(0)
+
+for i in range(len(narr)-1,pos,-1):
+    narr[i] = narr[i-1]
+
+narr[pos] = ele
+
+print(narr)
