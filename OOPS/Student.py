b7835308f72e0c43a4f40f172c10b21cf65cb871
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-01-12 15:27:11 +0530

    update

---
+++ OOPS/Student.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    def avgMarks(self):
+        sum = 0
+        for mark in self.marks:
+            sum += mark
+        return sum/3
+    
+    
+
+s1 = Student('Jeeban',[89,87,67])
+x = s1.avgMarks()
+print(x)
