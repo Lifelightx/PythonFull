68d9b3bd8f84d8f4c0ac0f165e0140eeb8fcd063
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-02-20 15:34:43 +0530

    update

--- DSA/STRINGS/reverse.py
+++ DSA/STRINGS/reverse.py
@@ -1,4 +1,21 @@
 str = "Hello Jeeban"
+revStr = ""
+for i in range(len(str)-1,-1,-1):
+    revStr += str[i]
+    
+print(revStr)
 
-x = str.find('o')
-print(x)
+
+def revString(s):
+    return s[::-1]
+
+print(revString("My Name is Laxan"))
+
+def revStr(s):
+    rev_str = ""
+    for char in s:
+        rev_str = char + rev_str
+    return rev_str
+
+print(revStr("MY name is Jeeban"))
+    
