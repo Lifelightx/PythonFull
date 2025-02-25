7fa852eba87a68965551cc4a05459bf725383be6
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-02-25 18:06:24 +0530

    update

---
+++ PATTERNS/striver/pattern.py
@@ -0,0 +1,13 @@
+"""
+*****
+*****
+*****
+*****
+*****    
+"""
+
+row = int(input("Enter no of rows: "))
+for i in range(row):
+    for j in range(row):
+        print("*", end='')
+    print()
---
+++ PATTERNS/striver/pattern1.py
@@ -0,0 +1,13 @@
+""" 
+*
+**
+***
+****
+*****
+"""
+
+rows = int(input("Enter rows: "))
+for i in range(rows):
+    for j in range(i+1):
+        print("*", end="")
+    print()
