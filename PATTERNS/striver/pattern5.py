66e21c1862bf72a1d27e8e5a518e2d7804bd91a4
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-02-26 18:15:40 +0530

    update

--- PATTERNS/striver/pattern5.py
+++ PATTERNS/striver/pattern5.py
@@ -6,9 +6,9 @@
 """
 
 rows = int(input("Enter rows: "))
-for i in range(rows):
-    for j in range(i):
+for i in range(rows,0,-1):
+    for j in range(rows-i):
         print(" ",end="")
-    for k in range(2*i+1):
+    for k in range(2*i-1):
         print("*",end="")
     print()
