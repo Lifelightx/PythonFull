01d8ccfc1980a1c4e531df55dad3c6624ae86959
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-02-27 08:26:07 +0530

    update

---
+++ FUNCTIONS/Collartz.py
@@ -0,0 +1,12 @@
+def collatz(num):
+    if num % 2 == 0:
+        return num // 2
+    else:
+        return num*3 +1
+    
+while True:
+    num = int(input("Enter number: "))
+    if num == 1:
+        break
+    else:
+        print(collatz(num))
