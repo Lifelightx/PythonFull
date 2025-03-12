76a4717062db07fadd85760efcc4b598937e67b1
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-03-12 19:17:27 +0530

    update

--- LISTS/RemoveDuplicates.py
+++ LISTS/RemoveDuplicates.py
@@ -5,7 +5,37 @@ lst = [2,4,6,5,5,4,6,2,4,1,3,7,0]
 lst1 = [1,2,3]
 lst2 = [4,5,6]
 
-l = lst1+lst2
+unique_list = []
 
-print(l)
+for i in lst:
+    if i not in unique_list:
+        unique_list.append(i)
+        
+        
+print(unique_list)
+
+#time complexcity O(n^2)
+#space complexcity O(n)
+
+""" 
+
+"""
+
+lst = [3,4,4,2,1,1,5]
+
+uniqueIndex = 1
+for i in range(1,len(lst)):
+    is_duplicate = False
+    for j in range(uniqueIndex):
+        if lst[i] == lst[j]:
+            is_duplicate = True
+            break
+    if not is_duplicate:
+        lst[uniqueIndex] = lst[i]
+        uniqueIndex +=1
+            
+
+         
+del lst[uniqueIndex:]
+print(lst)  
 
