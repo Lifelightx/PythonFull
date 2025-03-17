a976ebe61b58b8f0bcce2f96ab938c52031a36ff
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-03-17 21:37:48 +0530

    update

---
+++ FUNDAMENTALS/cathat.py
@@ -0,0 +1,9 @@
+str = "ziggazmy"
+
+cat = str.count('cat')
+hat = str.count('hat')
+
+if cat == hat:
+    print("cat and hat appears %s  number of times"%cat)
+else:
+    print("cat and hat are appears %s"%cat)
