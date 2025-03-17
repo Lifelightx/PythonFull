f3c88d9709b9843d55ac4d7c1eb310d7ecad1108
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-03-17 21:51:03 +0530

    update

---
+++ FUNDAMENTALS/printIndexesAtEven.py
@@ -0,0 +1,5 @@
+str = "heloJebanWelcomeTourCmpany"
+
+for i in range(len(str)):
+    if i % 2 == 0:
+        print(str[i], end="")
---
+++ FUNDAMENTALS/printNumbersIndesc.py
@@ -0,0 +1,5 @@
+def print_nums(num):
+    for i in range(num,0,-1):
+        print(i ,end=" ")
+        
+print_nums(8)
