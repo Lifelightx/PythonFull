0b757a54246d55a9b0b9c11aa5da225b26d1f9df
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-03-17 21:31:38 +0530

    update

---
+++ FUNDAMENTALS/troubleFriend.py
@@ -0,0 +1,15 @@
+""" 
+tt = t
+tf = f
+ft = f
+ff = f
+"""
+
+def trouble_friend(f1, f2):
+    if f1 == f2 :
+        print("Your are in trouble.")
+    else:
+        print("You are not in trouble")
+        
+        
+trouble_friend(False, False)
